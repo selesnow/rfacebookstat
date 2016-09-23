661a03e0d37c2f9daf27b3d0128be3a32b239b88
Alexey Seleznev <selesnow@mail.ru>
2016-09-23 11:35:01 +0300

    Update fbGetToken.R

--- R/fbGetToken.R
+++ R/fbGetToken.R
@@ -1,4 +1,8 @@
 fbGetToken <- function(app_id = NULL){
+  if(is.null(app_id)){
+    warning("Enter your App ID in argument app_id!")
+    return()
+  }
   link <- paste0("https://www.facebook.com/dialog/oauth?client_id=",app_id,"&display=popup& &redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=ads_read,business_management,manage_pages,ads_management")
   browseURL(link)
   token <- readline(prompt = "Enter your token: ")
