bc393a723244571321fb69590928cb1d295146c6
Alexey Seleznev <selesnow@mail.ru>
2016-09-23 10:53:34 +0300

    Update fbGetToken.R

--- R/fbGetToken.R
+++ R/fbGetToken.R
@@ -1,6 +1,6 @@
-fbGetToken <-
-function(){
-  browseURL("https://www.facebook.com/dialog/oauth?client_id=176943372709235&display=popup& &redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=ads_read,business_management,manage_pages,ads_management")
+fbGetToken <- function(app_id = NULL){
+  link <- paste0("https://www.facebook.com/dialog/oauth?client_id=",app_id,"&display=popup& &redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=ads_read,business_management,manage_pages,ads_management")
+  browseURL(link)
   token <- readline(prompt = "Enter your token: ")
   return(token)
 }
