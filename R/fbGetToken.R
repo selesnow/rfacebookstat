df30f49552179c1f3b46de078bd51415d3c23541
Alexey Seleznev <selesnow@mail.ru>
2017-09-22 15:56:43 +0300

    Update fbGetToken.R

--- R/fbGetToken.R
+++ R/fbGetToken.R
@@ -1,7 +1,7 @@
 fbGetToken <-
   function(app_id = NULL){
     if(is.null(app_id)) stop("Enter your app id.")
-    browseURL(paste0("https://www.facebook.com/dialog/oauth?client_id=",app_id  ,"&display=popup& &redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=ads_read,business_management,manage_pages,ads_management"))
+    browseURL(paste0("https://www.facebook.com/dialog/oauth?client_id=",app_id  ,"&display=popup&redirect_uri=https://selesnow.github.io/rfacebookstat/getToken/get_token.html&response_type=token&scope=ads_read,business_management,manage_pages,ads_management"))
     token <- readline(prompt = "Enter your token: ")
     return(token)
   }
