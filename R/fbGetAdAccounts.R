bdbf669da5482c440b4144e2e77eb66e518c43af
Alexey Seleznev <selesnow@mail.ru>
2017-10-10 17:20:45 +0300

    Add paginetion, and source_id
    In function add pagination mechanism, and change project id to source id.

--- R/fbGetAdAccounts.R
+++ R/fbGetAdAccounts.R
@@ -1,17 +1,40 @@
-fbGetAdAccounts <-
-function(projects_id = NULL, api_version = "v2.8",access_token = NULL){
-  result <- data.frame()
-  for(i in 1:length(projects_id)){
-    QueryString <- paste0("https://graph.facebook.com/",api_version,"/",projects_id[i],"/adaccounts?fields=name,id,account_id&access_token=",access_token)
-    answer <- getURL(QueryString)
-    answerobject <- fromJSON(answer)
-    tempData <- answerobject$data
-    result <- rbind(result, tempData)
-    if(!is.null(answerobject$error)) {
-      error <- answerobject$error
-      message(message(answerobject$error))
-    }
-    if(exists("tempData")){rm(tempData)}
+fbGetAdAccounts2 <- function(source_id = NULL, api_version = "v2.10", access_token = NULL){
+  
+  #Check of fill out access_token
+  if(is.null(access_token)){
+    stop("access_token id require argument!")
   }
-  return(result)
-}
+  
+  #Check source
+  if(is.null(source_id)){
+    source_id <- "me"
+  }
+  
+  #Create result data frame
+  result <- data.frame(stringsAsFactors = FALSE)
+  
+  #Compose query string
+  QueryString <- paste0("https://graph.facebook.com/",api_version,"/",source_id,"/adaccounts?&fields=name,id,account_id,account_status,user_role,age,business_name,amount_spent,balance,currency,business_city,business_country_code&access_token=",access_token)
+
+  #Send query to API server
+  answer <- GET(QueryString)
+
+  #Parse result
+  raw <- fromJSON(content(answer, "text", "application/json"))
+
+  #Check error
+  if(!is.null(raw$error)){
+    stop(raw$error$message)
+  }
+
+#Add to result
+result <- rbind(result, raw$data)
+
+#Paging
+while(!is.null(raw$paging$`next`)){
+  QueryString <- raw$paging$`next`
+  answer <- GET(QueryString)
+  raw  <- fromJSON(content(answer, "text", "application/json")) 
+  result <- rbind(result, raw$data)}
+
+return(result)}
