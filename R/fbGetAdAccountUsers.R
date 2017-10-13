fa12b12fd2b3bcccb9386e5e46e9287bfacd968e
Alexey Seleznev <selesnow@mail.ru>
2017-10-13 11:18:58 +0300

    Update fbGetAdAccountUsers.R

--- R/fbGetAdAccountUsers.R
+++ R/fbGetAdAccountUsers.R
@@ -19,53 +19,41 @@ accounts_id <- ifelse(grepl("^act_",accounts_id),accounts_id,paste0("act_",accou
 #result df
 result <- data.frame(stringsAsFactors = F)
 
-packageStartupMessage("Processing", appendLF = F)
+packageStartupMessage("Processing...", appendLF = T)
 #
 #start cycle
 for(account in accounts_id){
   if(is.na(account)|is.null(account)) next
-#ФОрмируем URL HTTP запроса
- packageStartupMessage(".", appendLF = F)
- QueryString <- paste0("https://graph.facebook.com/",api_version,"/",account,"/users?access_token=",access_token)
+  
+#Compose URL hhtp request
+ packageStartupMessage(account, appendLF = F)
+ 
+ QueryString <- paste0("https://graph.facebook.com/",api_version,"/",account,"/userpermissions?access_token=",access_token)
 
- #Отправляем запрос на сервер
+ #Send request
  answer <- GET(QueryString)
  raw <- fromJSON(content(answer, "text", "application/json",encoding = "UTF-8"))
  
- #Проверям результат на ошибки
+ #Check answer on error
  if(length(raw$error) > 0){
-   packageStartupMessage(paste0(account," - ",raw$error$code, " - ", raw$error$message), appendLF = T)
+   packageStartupMessage(paste0(" - ",raw$error$code, " - ", raw$error$message), appendLF = T)
    next
  }
 
- #Парсим ответ и преобразуем его в табицу
+ #Parse answer and transform him to data frame
  flatten_data <- fromJSON(content(answer, "text", "application/json",encoding = "UTF-8"), flatten = T)$data
- #Check account data
- if(length(flatten_data)==0){
-      packageStartupMessage(paste0(account," - Empty data."), appendLF = T)
-      next
+ 
+ if(is.null(flatten_data)|length(flatten_data) == 0){
+   packageStartupMessage(paste0(" - Empty userlist!"), appendLF = T)
+   next
  }
-  
+   
  flatten_data$account_id <- account
  
- #Получаем имя пользователя и ссылку на его страницу
- for(user in flatten_data$id){
-  
-  QueryString <- paste0("https://graph.facebook.com/",api_version,"/",user,"?fields=id,name,link&access_token=",access_token)
-  answerUSER <- GET(QueryString)
-  stop_for_status(answerUSER)
-  raw <- fromJSON(content(answerUSER, "text", "application/json",encoding = "UTF-8"))
-  #Проверям результат на ошибки
-  if(length(answerUSER$error) > 0){
-    packageStartupMessage(paste0(account," - ",raw$error$code, " - ", raw$error$message), appendLF = T)
-    next
-  }
-
-  rawUSER <- data.frame(fromJSON(content(answerUSER, "text", "application/json",encoding = "UTF-8"), flatten = T))
-  flatten_data$link[flatten_data$id == user] <- as.character(rawUSER$link)
-  }
-
+ #Add to result data.frame
  result <- rbind(result, flatten_data)
+ 
+ packageStartupMessage(paste0(" - Done, ",length(unique(flatten_data$user.id))," users"), appendLF = T)
 }
 
 #back string as factor value
