18f2cf98e0919469267c53cbf12bc4e0babb98a3
Alexey Seleznev <selesnow@mail.ru>
2017-12-22 10:50:13 +0200

    Migrate to support v2.11 and 2.10

--- R/fbGetAdAccounts.R
+++ R/fbGetAdAccounts.R
@@ -1,4 +1,6 @@
-fbGetAdAccounts <- function(source_id = NULL, api_version = "v2.11", access_token = NULL){
+fbGetAdAccounts <- function(source_id = NULL, 
+                            api_version = "v2.11", 
+                            access_token = NULL){
   
   #Check of fill out access_token
   if(is.null(access_token)){
@@ -10,34 +12,46 @@ fbGetAdAccounts <- function(source_id = NULL, api_version = "v2.11", access_toke
     source_id <- "me"
   }
   
+  #Define endpoint, joint operation with v2.11 and v2.10
+  if(api_version == "v2.10"){
+    end_points <- "adaccounts"
+  } else {
+    end_points <- c("owned_ad_accounts","client_ad_accounts")
+  }
+  
   #Create result data frame
   result <- data.frame(stringsAsFactors = FALSE)
   
+  #start endpoint_cycle
+  for(end_point in end_points){
   #Compose query string
-  QueryString <- paste0("https://graph.facebook.com/",api_version,"/",source_id,"/adaccounts?&fields=name,id,account_id,account_status,user_role,age,business,owner,partner,amount_spent,spend_cap,balance,currency,business_city,business_country_code&limit=100&access_token=",access_token)
-
+  QueryString <- paste0("https://graph.facebook.com/",api_version,"/",source_id,"/",end_point,"?fields=name,id,account_id,account_status,user_role,age,business,owner,partner,amount_spent,spend_cap,balance,currency,business_city,business_country_code&limit=100&access_token=",access_token)
+  
   #Send query to API server
   answer <- GET(QueryString)
-
+  
   #Parse result
   raw <- fromJSON(content(answer, "text", "application/json",encoding = "UTF-8"),flatten = T)
-
+  
   #Check error
   if(!is.null(raw$error)){
     stop(raw$error$message)
   }
-
-#Add to result
-result <- rbind(result, raw$data)
-
-#Paging
-while(!is.null(raw$paging$`next`)){
-  QueryString <- raw$paging$`next`
-  answer <- GET(QueryString)
-  raw  <- fromJSON(content(answer, "text", "application/json",encoding = "UTF-8"),flatten = T) 
-  result <- rbind(result, raw$data)}
-
-#Transform to numeric
-result$balance <- result$balance / 100
-result$amount_spent <- result$amount_spent / 100
-return(result)}
+  
+  #Add to result
+  result <- rbind(result, raw$data)
+  
+  #Paging
+  while(!is.null(raw$paging$`next`)){
+    QueryString <- raw$paging$`next`
+    answer <- GET(QueryString)
+    raw  <- fromJSON(content(answer, "text", "application/json",encoding = "UTF-8"),flatten = T) 
+    result <- rbind(result, raw$data)}
+  }
+  
+  #Transform to numeric
+  result$balance <- as.numeric(result$balance)
+  result$balance <- result$balance / 100
+  result$amount_spent <- as.numeric(result$amount_spent)
+  result$amount_spent <- result$amount_spent / 100
+  return(result)}
