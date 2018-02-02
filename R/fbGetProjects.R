8e9a84bcbb0ba06602fe4f0e18155d0d2e56bed5
Alexey Seleznev <selesnow@mail.ru>
2018-02-02 09:48:08 +0200

    Migrate to api_version = "v2.12"

--- R/fbGetProjects.R
+++ R/fbGetProjects.R
@@ -1,5 +1,5 @@
 fbGetProjects <-
-function(bussiness_id = NULL, api_version = "v2.11", access_token = NULL){
+function(bussiness_id = NULL, api_version = "v2.12", access_token = NULL){
   
   QueryString <- paste0("https://graph.facebook.com/",api_version,"/",bussiness_id,"/businessprojects?access_token=",access_token)
   answer <- getURL(QueryString)
