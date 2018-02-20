f92d4ae9624ae9efe164dac9fa5b9e6301c4da03
Alexey Seleznev <selesnow@mail.ru>
2018-02-20 10:12:09 +0200

    Add files via upload

---
+++ R/fbGetAdAccountUsersPermissions.R
@@ -0,0 +1,46 @@
+fbGetAdAccountUsersPermissions <- function(accounts_id = NULL ,
+                                           api_version = "v2.12", 
+                                           console_type = "progressbar",
+                                           access_token = NULL){
+  
+  if(is.null(accounts_id)|is.null(access_token)){
+    stop("Arguments accounts_id and access_token is require.")
+  }
+  
+  #Create
+  result <- data.frame(stringsAsFactors = F)
+  
+  accounts_id <- ifelse(grepl("^act_",accounts_id),accounts_id,paste0("act_",accounts_id))
+  
+  #Progress settings
+  pb_step <- 1
+  
+  if(console_type == "progressbar" & length(accounts_id) > 1){
+    pb <- txtProgressBar(pb_step, length(accounts_id), style = 3)}
+  else{
+    console_type <- "message"
+  }
+  
+  for(account in accounts_id){
+    
+    QueryString <- paste0("https://graph.facebook.com/",api_version,"/",account,"/users","?access_token=",access_token)
+    ans <- GET(QueryString)
+    ans_pars <- content(ans)
+    
+    if(console_type == "progressbar"){
+      pb_step <- pb_step + 1
+      setTxtProgressBar(pb, pb_step)}
+    
+    if(length(ans_pars$data) == 0) next
+    
+    for(row in 1:length(ans_pars$data)){
+      result <- rbind(result,
+                      data.frame(name = ans_pars$data[[row]]$name,
+                                 id   = ans_pars$data[[row]]$id,
+                                 role = ans_pars$data[[row]]$role,
+                                 permissions = paste0(ans_pars$data[[row]]$permissions, collapse = ","),
+                                 account_id = account))
+    }
+  }
+  return(result)
+}
