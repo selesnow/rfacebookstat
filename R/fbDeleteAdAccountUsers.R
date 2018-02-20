32abe7f4f23e5cbf5e0a2ed61b5a8254799586a5
Alexey Seleznev <selesnow@mail.ru>
2018-02-20 12:59:34 +0200

    Add files via upload

---
+++ R/fbDeleteAdAccountUsers.R
@@ -0,0 +1,17 @@
+fbDeleteAdAccountUsers <-  function(user_ids = NULL, accounts_id = NULL,api_version = "v2.12",access_token = NULL){
+  
+  if(is.null(accounts_id)|is.null(access_token)){
+    stop("Arguments accounts_id and access_token is require.")
+  }
+  
+  for(account_id in accounts_id){
+    
+    for(uid in user_ids){
+      print(paste0("Account ",account_id))
+      QueryString <- paste0("https://graph.facebook.com/",api_version,"/",account_id,"/users/",uid,"?access_token=",access_token)
+      ans <- DELETE(QueryString)
+      ans <- content(ans)
+      print(ans)
+    }
+  }
+}
---
+++ R/fbUpdateAdAccountUsers.R
@@ -0,0 +1,35 @@
+fbUpdateAdAccountUsers <- function(user_ids = NULL, 
+                                   role = "advertiser", 
+                                   accounts_id = NULL,
+                                   api_version = "v2.12",
+                                   access_token = NULL){
+  
+  #Check account_id, token and uid
+  if(is.null(accounts_id)|is.null(access_token)|is.null(uid)){
+    stop("Arguments user_ids, accounts_id and access_token is require.")
+  }
+  
+  #Check role
+  role <- switch(tolower(role), 
+                 "administator" = 1001,
+                 "advertiser"   = 1002,
+                 "ad manager"   = 1002,
+                 "analyst"      = 1003,
+                 "sales"        = 1004,
+                 "direct sales" = 1004,
+                 tolower(role))
+  
+  for(account_id in accounts_id){
+    
+    for(uid in user_ids){
+      print(paste0("Add user ", uid, " to account ", account_id))
+      #Compose query
+      QueryString <- paste0("https://graph.facebook.com/",api_version,"/",account_id,"/users?uid=",uid,"&role=",role,"&access_token=",access_token)
+      #Send query
+      ans <- POST(QueryString)
+      #Parse query
+      ans <- content(ans)
+      #Message
+      print(ans)}
+  }
+}
