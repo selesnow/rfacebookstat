2b7da583f3e9b1008c96ab60d41248f0ca9c5dab
Alexey Seleznev <selesnow@mail.ru>
2018-08-08 16:21:14 +0300

    Add files via upload

--- R/fbGetAdAccountUsersPermissions.R
+++ R/fbGetAdAccountUsersPermissions.R
@@ -1,44 +1,43 @@
 fbGetAdAccountUsersPermissions <- function(accounts_id = NULL ,
-                                           api_version = "v3.1", 
+                                           api_version = "v3.1",
                                            console_type = "progressbar",
                                            access_token = NULL){
-  
+
   if(is.null(accounts_id)|is.null(access_token)){
     stop("Arguments accounts_id and access_token is require.")
   }
-  
+
   #Create
   result <- data.frame(stringsAsFactors = F)
-  
+
   accounts_id <- ifelse(grepl("^act_",accounts_id),accounts_id,paste0("act_",accounts_id))
-  
+
   #Progress settings
   pb_step <- 1
-  
+
   if(console_type == "progressbar" & length(accounts_id) > 1){
-    pb <- txtProgressBar(pb_step, length(accounts_id), style = 3)}
+    pb <- utils::txtProgressBar(pb_step, length(accounts_id), style = 3)}
   else{
     console_type <- "message"
   }
-  
+
   for(account in accounts_id){
-    
+
     QueryString <- paste0("https://graph.facebook.com/",api_version,"/",account,"/users","?access_token=",access_token)
-    ans <- GET(QueryString)
-    ans_pars <- content(ans)
-    
+    ans <- httr::GET(QueryString)
+    ans_pars <- httr::content(ans)
+
     if(console_type == "progressbar"){
       pb_step <- pb_step + 1
-      setTxtProgressBar(pb, pb_step)}
-    
+      utils::setTxtProgressBar(pb, pb_step)}
+
     if(length(ans_pars$data) == 0) next
-    
+
     for(row in 1:length(ans_pars$data)){
       result <- rbind(result,
                       data.frame(name = ans_pars$data[[row]]$name,
                                  id   = ans_pars$data[[row]]$id,
-                                 role = ans_pars$data[[row]]$role,
-                                 permissions = paste0(ans_pars$data[[row]]$permissions, collapse = ","),
+                                 role = paste0(ans_pars$data[[row]]$tasks, collapse = ", "),
                                  account_id = account))
     }
   }
