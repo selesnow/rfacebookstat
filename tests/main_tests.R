0bfb90b4ad80dbff4b1e8851111e5f40b410d6d0
Alexey Seleznev <selesnow@gmail.com>
2021-04-16 15:56:03 +0300

    Migrate to API v10.0
    Namespace, News, Description, Vignetts etc

--- tests/main_tests.R
+++ tests/main_tests.R
@@ -1,42 +1,48 @@
 library(rfacebookstat)
 library(dplyr)
 
+# accounts
 bm <- fbGetBusinessManagers()
 my_acs      <- fbGetAdAccounts()
 bm_accounts <- fbGetAdAccounts(bm$id[1])
 test_accs   <- sample_n(bm_accounts, size = 10, replace = FALSE)$id
-
-camp   <- fbGetCampaigns(test_accs)
-adsets <- fbGetAdSets(test_accs)
-ads    <- fbGetAds(test_accs)
-creo   <- fbGetAdCreative(test_accs)
-apps   <- fbGetApps()
-pages  <- fbGetPages(test_accs)
-videos <- fbGetAdVideos(my_acs$id[4:10])
+# objects
+camp        <- fbGetCampaigns(test_accs)
+adsets      <- fbGetAdSets(test_accs)
+ads         <- fbGetAds(test_accs)
+creo        <- fbGetAdCreative(test_accs)
+apps        <- fbGetApps()
+pages       <- fbGetPages(test_accs)
+videos      <- fbGetAdVideos(my_acs$id[4:10])
 conversions <- fbGetAdAccountsConversions()
-as_tibble(pages)
+# users
+bm_users         <- fbGetBusinessManagersUsers(bm$id[1])
+bm_user_accounts <- fbGetBusinessUserAdAccounts(bm_users$id[7], business_id = bm$id[1])
+# statistic
+simple_stat <- fbGetMarketingStat(
+  test_accs, 
+  date_start = Sys.Date() - 12, 
+  date_stop = Sys.Date() - 1)
+action_stat <- fbGetMarketingStat(
+  test_accs,
+  level  = "adset",
+  fields = "campaign_name,
+            adset_name,
+            impressions,
+            clicks,
+            reach,
+            spend,
+            actions,
+            action_values",
+  date_preset = 'last_7d', 
+  breakdowns  = 'publisher_platform',
+  attribution_window = c('1d_view','7d_click'),
+  action_breakdowns = "action_type"
+)
 
-fb_data.disable_reason <-
-tibble(reason_id   = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
-       reason_name = c("NONE",
-                       "ADS_INTEGRITY_POLICY",
-                       "ADS_IP_REVIEW",
-                       "RISK_PAYMENT",
-                       "GRAY_ACCOUNT_SHUT_DOWN",
-                       "ADS_AFC_REVIEW",
-                       "BUSINESS_INTEGRITY_RAR",
-                       "PERMANENT_CLOSE",
-                       "UNUSED_RESELLER_ACCOUNT",
-                       "UNUSED_ACCOUNT"))
-
-save(fb_data.disable_reason, file = "D:/packlab/github/testspace/rfacebookstat/R/sysdata.rda")
-
-  fbGetToken
-  
-  library(httr)
-  tkn <- fbAuth()
-  ans <- DELETE("https://graph.facebook.com/1246563312029308/permissions?access_token=EAACg7dbgLXMBACgDSVdhZAHjNY6RsxZC59csSGZB1hMMPe9yvH2qd5oEgwgiyL3HHKpdObrVZAkRJnZC7NlPxXAUy8qXe6jVFgZCwHA3zax6UCz8xKDYF4gFTNyZCtflddTZAPBDaJZBwh7JbuQIyv6n08Lh7EhGBmkWGZBxy3NONhnwT14HoVw6Ng
-")
-  ans2 <- content(ans,as = "parse", "text/html" )
-  
-  print()
+# auth tets
+# owner
+fbAuth('selesnow')
+# test user
+fbAuth('alexey_dxqanrl_tester@tfbnw.net')
+rfacebookstat:::fbRevokeAppPrivilegies()
